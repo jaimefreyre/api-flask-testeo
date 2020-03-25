5710663cf863723645aef631135901ba2eed1844
jaimefreyre <estudiobudapest@gmail.com>
2020-03-25 04:34:56 -0300

    V2

---
+++ verifi.py
@@ -0,0 +1,8 @@
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id) # Fetch the user from the database
+
