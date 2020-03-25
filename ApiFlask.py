da241c6e7c70cce685d5c5bff02721ce8d6a3d6d
jaimefreyre <estudiobudapest@gmail.com>
2020-03-25 03:18:27 -0300

    V1

--- ApiFlask.py
+++ ApiFlask.py
@@ -1,14 +1,34 @@
-from flask import Flask, jsonify
+import os
+from werkzeug import secure_filename
+from flask import Flask, jsonify, render_template, request
 
+# instancia del objeto Flask
 app = Flask(__name__)
+
+# Carpeta de subida
+app.config['UPLOAD_FOLDER'] = './Archivos PDF'
 
 @app.route('/')
 def inicio():
 	return jsonify({"mensaje": "Api rest hecha en Python Flask : Direcciones: /pingo, /items, /item/<id>"})
 
-@app.route('/pingo')
-def pingo():
-	return jsonify({"mensaje": "Api rest hecha en Python PINGO, desplegada para tester rapidos"})
+
+
+@app.route("/subir")
+def upload_file():
+ # renderiamos la plantilla "forma1.html"
+ return render_template('forma1.html')
+
+@app.route("/upload", methods=['POST'])
+def uploader():
+ if request.method == 'POST':
+  # obtenemos el archivo del input "archivo"
+  f = request.files['archivo']
+  filename = secure_filename(f.filename)
+  # Guardamos el archivo en el directorio "Archivos PDF"
+  f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  # Retornamos una respuesta satisfactoria
+  return "<h1>Archivo subido exitosamente</h1>"
 
 @app.route('/items')
 def items():
